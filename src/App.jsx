8d2ab7eb4b0daf8b09d24592210bcf56f3c2759c
Mohamed Amarcha <simoamarcha@icloud.com>
2023-06-23 02:24:51 +0100

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Home />
       <Skill />
       <Work />
-      <Home />
+      {/* <Home /> */}
     </>
   )
 }
--- src/Work.jsx
+++ src/Work.jsx
@@ -26,12 +26,12 @@ export default function Work() {
                     </div>
                     {/* <img src={line} className='line3 ml-36' /> */}
 
-                    <div className='project flex justify-between items-center'>
+                    <div className='project pro2 flex justify-between items-center'>
                         <div className='desc flex flex-col justify-center'>
                             <h3 className='featured'>Featured Project</h3>
                             <h2 className='title'>Spolaya Fragrance</h2>
                             <div className='box'>
-                                <p className='description'>Mesmerizing Front-End Design, Unleash Your Senses. Indulge in a sensory journey through our captivating fragrance shop website. Immerse yourself in an enchanting website that captures the essence of scent and allure.</p>
+                                <p className='description'>Mesmerizing Front-End Design, Unleash Your Senses. Indulge in a sensory journey through our captivating fragrance shop website. Immerse in an enchanting website that captures the essence of scent and allure.</p>
                             </div>
                             <div className='stacks flex'>
                                 <h4 className='stackk'>Firebase</h4>
